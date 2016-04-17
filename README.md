8df90118c998a44e18b17886228b59e4d60ddd1d
li-qiang <kata.Qlee@gmail.com>
2016-04-17 13:05:05 +0800

    Koa 2 init

---
+++ README.md
@@ -0,0 +1,5 @@
+### Develope
+
+```
+  npm run dev
+```
---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import Koa from 'koa';
+
+const app = new Koa();
+
+// uses async arrow functions
+app.use(async(ctx, next) => {
+  try {
+    await next(); // next is now a function
+  } catch (err) {
+    ctx.body = {
+      message: err.message
+    };
+    ctx.status = err.status || 500;
+  }
+});
+
+app.use(async ctx => {
+  const user = await Haha.getById(ctx.session.userid); // await instead of yield
+  ctx.body = user; // ctx instead of this
+});
+
+app.listen(3000);
